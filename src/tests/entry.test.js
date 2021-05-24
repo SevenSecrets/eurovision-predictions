c7f8dcacd8bcae1546ba7dd68a107febcf3acfff
Art <freddieshughes@googlemail.com>
2021-05-24 16:15:45 +0100

    fixing entry test

--- src/tests/entry.test.js
+++ src/tests/entry.test.js
@@ -3,7 +3,7 @@ import Entry from '../components/entry';
 
 it('renders with props', () => {
     render(<Entry songTitle='SHUM' artist='Go_A' country='Ukraine' rank="1" />);
-    expect(screen.getByText('SHUM')).toBeInTheDocument();
-    expect(screen.getByText('Go_A')).toBeInTheDocument();
+    expect(screen.getByText('Song: SHUM')).toBeInTheDocument();
+    expect(screen.getByText('Artist: Go_A')).toBeInTheDocument();
     expect(screen.getByText('Ukraine')).toBeInTheDocument();
 });

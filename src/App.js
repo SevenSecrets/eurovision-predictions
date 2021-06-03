9e551b31344b947b209cbf106f994512b6cc345d
Art <freddieshughes@googlemail.com>
2021-06-03 11:47:46 +0100

    rendering list of entries fetched from store

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Entry from './components/entry';
 import EntryForm from './components/entryForm';
 import './stylesheets/App.css';
 
-function App(props) {
+function App() {
+
+  const entryList = useSelector(state => state.entries.entries);
+
+  const renderedEntryList = entryList.map((entry, i) => (
+    <li key={i}>
+      <Entry songTitle={entry.songTitle} artist={entry.artist} country={entry.country} rank={i + 1} />
+    </li>
+  ));
+
   return(
     <div>
       <header>
         <h1>Build Your Own Eurovision Rankings Predictions</h1>
       </header>
       <EntryForm />
+      <ul>
+        {renderedEntryList}
+      </ul>
     </div>
   );
 }

38cbe19a3bd25ac24a8a90a704fa6defdb864bdc
Art <freddieshughes@googlemail.com>
2021-06-03 11:19:17 +0100

    switching App to a functional component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Entry from './components/entry';
 import EntryForm from './components/entryForm';
 import './stylesheets/App.css';
 
-class App extends Component {
-  render() {
-    return(
-      <div>
-        <header>
-          <h1>Build Your Own Eurovision Rankings Predictions</h1>
-        </header>
-        <EntryForm />
-        <Entry songTitle='SHUM' artist='Go_A' country='Ukraine' rank="1" />
-      </div>
-    );
-  }
+function App(props) {
+  return(
+    <div>
+      <header>
+        <h1>Build Your Own Eurovision Rankings Predictions</h1>
+      </header>
+      <EntryForm />
+    </div>
+  );
 }
 
 export default App;
